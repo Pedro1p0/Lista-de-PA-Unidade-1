e6d1a9a552333b40b0f75ad9af7206b5877a29f1
Pedro1p0 <93002195+Pedro1p0@users.noreply.github.com>
2021-11-18 03:41:42 -0300

    Update Questão-12.c

--- Questão-12.c
+++ Questão-12.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 
 /*
-basicamente ponteiros são imporantes para funções porque na hora
-de modificar variaveis recebidas pela função main, elas serão
-alterados em seus endereços de memória e não apenas numa variavel
-provisória criada pela função, o que pode causar o não funcionamento
-daquela função como deveria, por exemplo, irei criar uma função que adiciona
+basicamente ponteiros são imporantes porque na hora
+de modificar variaveis recebidas pela função main em outra função auxiliar, elas serão
+alteradas em seus endereços de memória e não apenas em uma variavel
+provisória criada pela função, já que quando a execução da função finaliza essa variavel provisória é apagada, 
+o que pode causar o não funcionamento daquela função como planejado,
+por exemplo, irei criar uma função que adiciona
 10 no valor de uma variavel, se a variavel for passada sem o uso de ponteiros
-ela será alterada na função no entanto logo que a função acabar de executar
-será apagada com nada na variavel da função principal tendo sido alterado, só que
+ela será alterada na função no entanto logo que a função acaba a sua execução ela
+será apagada, com a modificação na variavel da função principal nunca existindo, só que
 quando passamos por ponteiro a função altera no endereço da variavel, assim mesmo quando a função
-acaba de executar, a sua tarefa foi concluida e a variavel da função principal foi alterada.
+acaba de executar, a sua tarefa foi concluida e a variavel da função principal foi alterada. 
+o que torna a linguagem muito flexivel podendo alterar valores diretamente em seus endereços de memória.
 */
 void soma10(int *a){
 int aux;
@@ -25,9 +27,6 @@ int main()
 {
     int x = 30;
     printf("digite o valor de X \n");
-    //scanf("%d",x);
-    //somadez(x);
-    //printf("o valor digitado alterado eh %d",x);
     soma10(&x);
     printf(" \n retornando para a funcao principal temos que o valor foi alterado %d",x);
         return 0;
