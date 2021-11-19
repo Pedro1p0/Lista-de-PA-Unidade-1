cbc88d998c46004f4f93ed33032d6c79dfcc67fe
Pedro1p0 <93002195+Pedro1p0@users.noreply.github.com>
2021-11-19 11:22:57 -0300

    Update Questão-14.c

--- Questão-14.c
+++ Questão-14.c
@@ -4,6 +4,7 @@
 #include <time.h> // necessário para a resposta da questão 16
 
 
+
 int comparar(const void *x,const void *y) // função comparador irá nos ajudar a determinar a ordem dos elementos
 {
     return (*(int*)x-*(int*)y); // a função irá ordenar os valores de x e y em ordem crescente
@@ -23,17 +24,24 @@ int main()
     {
         scanf("%f",&vetor[i]); // capturando os valores do vetor com a ajuda do for
     }
-    long double tempo=0; // questão 16
-    clock_t t1 = clock(); // questão 16
-    qsort(vetor,n,sizeof(float),comparar); // função de ordenação dos valores
-    clock_t auxt1 = clock(); // questão 16
+     // questão 16
 
-    tempo = (auxt1 - t1); // questão 16
+
+    clock_t t1,tempo;
+    t1 = clock(); // questão 16
+    qsort(vetor,n,sizeof(float),comparar); // função de ordenação dos valores
+
+    t1 = (clock()-t1); // questão 16
+
+    printf(" tempo de execucao %f milisegundos \n", ((long double)t1)/((CLOCKS_PER_SEC/1000)));
+
     printf(" os valores ordenados sao \n");
+
     for (int i=0; i<n; i++) // for para a leitura dos dados
     {
         printf("%f\n",vetor[i]); // exibição ordenada do vetor
     }
-    printf("%Lf",tempo); // questão 16
+    free(vetor);
+
     return 0; // retorno da função principal
 }
